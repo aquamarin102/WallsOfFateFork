298ee88334ab0e2a705f44d3ed066d9e5f1eaa4f
aquamarin102 <aquamarin120@gmail.com>
2026-04-17 12:55:29 +0300

    Squashed commit of the following:
    commit c844d426b20ce8ccf5506584df7df8ae5fc04470
    Merge: 413efc7 ccbdf56
    Author: Dobruy_Pez <138577710+DobruyPez@users.noreply.github.com>
    Date:   Wed Apr 15 18:29:16 2026 +0300
    
        Merge branch 'dev' of github.com:DobruyPez/WallsOfFate into dev
    
        # Conflicts:
        #	Assets/Resources/Characters/Messenger.prefab
    
    commit 413efc7f8e664c598784100743cc6498875cd99e
    Author: Dobruy_Pez <138577710+DobruyPez@users.noreply.github.com>
    Date:   Wed Apr 15 18:28:22 2026 +0300
    
        fixed missed links in quests & dialogs
    
    commit ccbdf56ce0920ba28f63b7038bb6e0e4e9cbbd9c
    Author: aquamarin102 <97702538+aquamarin102@users.noreply.github.com>
    Date:   Tue Apr 14 16:08:49 2026 +0300
    
        FixesOnScenes & CameraObstacleTransparency

---
+++ Assets/_Project/Scripts/Scenes/Storage/StorageController.cs
@@ -0,0 +1,203 @@
+using Game.Data;
+using UnityEngine;
+using Zenject;
+
+namespace Game
+{
+    internal class StorageController : MonoBehaviour
+    {
+        [SerializeField] private DialogManager dialogueManager;
+        [SerializeField] private InteractiveItemHandler interactiveItemHandler;
+        [SerializeField] private string ThiefPrefabName;
+
+        private QuestManager questManager;
+        private NPCPrefabFactory npcPrefabFactory;
+        private bool initialSceneStateApplied;
+        private bool subscribedToDialogueEvents;
+        private bool subscribedToItemEvents;
+
+        [Inject]
+        private void Construct(QuestManager questManager, NPCPrefabFactory factory)
+        {
+            this.questManager = questManager;
+            this.npcPrefabFactory = factory;
+        }
+
+        private void Start()
+        {
+            TrySubscribeToSceneEvents();
+            TryApplyInitialSceneState();
+        }
+
+        private void Update()
+        {
+            if (initialSceneStateApplied &&
+                subscribedToDialogueEvents &&
+                (interactiveItemHandler == null || subscribedToItemEvents))
+            {
+                return;
+            }
+
+            TrySubscribeToSceneEvents();
+            TryApplyInitialSceneState();
+        }
+
+        private void OnDestroy()
+        {
+            if (subscribedToDialogueEvents && dialogueManager != null)
+            {
+                dialogueManager.OnFinished -= OnDialogueFinished;
+            }
+
+            if (subscribedToItemEvents && interactiveItemHandler != null)
+            {
+                interactiveItemHandler.OnItemHandled -= OnQuestItemInteraction;
+            }
+        }
+
+        private void TryApplyInitialSceneState()
+        {
+            if (initialSceneStateApplied || questManager == null)
+            {
+                return;
+            }
+
+            Quest thiefQuest = questManager.GetQuest(1);
+            if (thiefQuest != null && questManager.GetQuestState(thiefQuest.Id) == QuestState.InProgress)
+            {
+                GameObject thiefNpc = FindNpcInstance(ThiefPrefabName);
+                if (thiefNpc != null)
+                {
+                    thiefNpc.SetActive(false);
+                }
+            }
+
+            initialSceneStateApplied = true;
+        }
+
+        public void OnDialogueFinished(DialogGraph dialogue)
+        {
+            if (dialogue == null)
+            {
+                return;
+            }
+
+            if (dialogue.Name == "Thief")
+            {
+                Quest thiefQuest = questManager.GetQuest(1);
+                if (thiefQuest == null)
+                {
+                    return;
+                }
+
+                questManager.UpdateQuest(thiefQuest.Id, QuestState.InProgress);
+                QuestTask task = questManager.GetQuestTask(thiefQuest.Id, 0);
+                if (task != null)
+                {
+                    questManager.UpdateQuestTask(thiefQuest.Id, task.Id, QuestState.InProgress);
+                }
+            }
+        }
+
+        public void OnQuestItemInteraction(InteractableItemParameters itemParameters)
+        {
+            if (itemParameters == null)
+            {
+                return;
+            }
+
+            if (itemParameters.ItemName == "Pouch")
+            {
+                Quest herbalistQuest = questManager.GetQuest(2);
+                if (herbalistQuest == null)
+                {
+                    return;
+                }
+
+                questManager.UpdateQuest(herbalistQuest.Id, QuestState.InProgress);
+                QuestTask task = questManager.GetQuestTask(herbalistQuest.Id, 0);
+                if (task != null)
+                {
+                    questManager.UpdateQuestTask(herbalistQuest.Id, task.Id, QuestState.InProgress);
+                }
+            }
+            if (itemParameters.ItemName == "Key")
+            {
+                Quest keyMasterQuest = questManager.GetQuest(3);
+                if (keyMasterQuest != null && questManager.GetQuestState(keyMasterQuest.Id) == QuestState.InProgress)
+                {
+                    QuestTask task = questManager.GetQuestTask(keyMasterQuest.Id, 0);
+                    if (task != null)
+                    {
+                        questManager.UpdateQuestTask(keyMasterQuest.Id, task.Id, QuestState.Completed);
+                    }
+
+                    QuestTask task1 = questManager.GetQuestTask(keyMasterQuest.Id, 1);
+                    if (task1 != null)
+                    {
+                        questManager.UpdateQuestTask(keyMasterQuest.Id, task1.Id, QuestState.InProgress);
+                    }
+                }
+            }
+            if (itemParameters.ItemName == "Scroll")
+            {
+                Quest messengerQuest = questManager.GetQuest(4);
+                if (messengerQuest == null)
+                {
+                    return;
+                }
+
+                questManager.UpdateQuest(messengerQuest.Id, QuestState.InProgress);
+                QuestTask task = questManager.GetQuestTask(messengerQuest.Id, 0);
+                if (task != null)
+                {
+                    questManager.UpdateQuestTask(messengerQuest.Id, task.Id, QuestState.InProgress);
+                }
+            }
+        }
+
+        private void TrySubscribeToSceneEvents()
+        {
+            if (!subscribedToDialogueEvents && dialogueManager != null)
+            {
+                dialogueManager.OnFinished += OnDialogueFinished;
+                subscribedToDialogueEvents = true;
+            }
+
+            if (!subscribedToItemEvents && interactiveItemHandler != null)
+            {
+                interactiveItemHandler.OnItemHandled += OnQuestItemInteraction;
+                subscribedToItemEvents = true;
+            }
+        }
+
+        private GameObject FindNpcInstance(string npcName)
+        {
+            if (string.IsNullOrWhiteSpace(npcName))
+            {
+                return null;
+            }
+
+            if (npcPrefabFactory != null && npcPrefabFactory.HasInstance(npcName))
+            {
+                return npcPrefabFactory.GetInstance(npcName);
+            }
+
+            GameObject[] rootObjects = gameObject.scene.GetRootGameObjects();
+            for (int index = 0; index < rootObjects.Length; index++)
+            {
+                Transform[] children = rootObjects[index].GetComponentsInChildren<Transform>(true);
+                for (int childIndex = 0; childIndex < children.Length; childIndex++)
+                {
+                    Transform child = children[childIndex];
+                    if (child != null && child.name == npcName)
+                    {
+                        return child.gameObject;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
